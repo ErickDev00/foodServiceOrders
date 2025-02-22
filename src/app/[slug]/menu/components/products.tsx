ce8ed9b7dfc6780dd94c1f2115e2c681b4b40133
Erick Rodrigues <profissional.erick@gmail.com>
2025-02-22 17:05:45 -0300

    fix bugs and final version

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -1,31 +1,35 @@
-import { Product } from "@prisma/client"
-import Image from "next/image"
-import Link from "next/link"
-import { useParams } from "next/navigation"
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+import { useParams, useSearchParams } from "next/navigation";
+
+import { formatCurrency } from "@/helpers/format-currency";
 
 interface ProductsProps {
-  products: Product[]
+  products: Product[];
 }
 
-
-
-
-
-const Products = ({products}: ProductsProps) => {
-  const {slug} = useParams<{ slug: string }>()
+const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
+  const searchParams = useSearchParams();
+  const consumptionMethod = searchParams.get("consumptionMethod");
   return (
     <div className="space-y-3 px-5">
-      {products.map(product => (
-        <Link key={product.id} href={`/${slug}/menu/${product.id}`} className="flex items-center justify-between gap-10 py-3 border-b">
+      {products.map((product) => (
+        <Link
+          key={product.id}
+          href={`/${slug}/menu/${product.id}?consumptionMethod=${consumptionMethod}`}
+          className="flex items-center justify-between gap-10 border-b py-3"
+        >
+          {/* ESQUERDA */}
           <div>
-            <h3 className="text-sm font-medium">
-              {product.name}
-            </h3>
-            <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
-            <p className="pt-3 text-sm font-semibold">{new Intl.NumberFormat("pt-br", {
-              style: "currency",
-              currency: "BRL"
-            }).format(product.price)}</p>
+            <h3 className="text-sm font-medium">{product.name}</h3>
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
+              {formatCurrency(product.price)}
+            </p>
           </div>
 
           {/* DIREITA */}
@@ -37,19 +41,10 @@ const Products = ({products}: ProductsProps) => {
               className="rounded-lg object-contain"
             />
           </div>
-
-
         </Link>
-
       ))}
-        
-          
-      
-
-
     </div>
-  
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
